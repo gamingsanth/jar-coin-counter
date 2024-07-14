764c5f1bd914bb8cb500ac008e810249691c8c2b
Santhosh Kumar <santhoshkumar@Santhoshs-MacBook-Pro.local>
2024-07-15 01:21:49 +0530

    change server url

--- src/components/Jar/JarCoinCounter.js
+++ src/components/Jar/JarCoinCounter.js
@@ -3,6 +3,7 @@ import Jar from "./Jar";
 import Button from "./IconButton";
 import AddIcon from '@mui/icons-material/Add';
 import axios from "axios";
+import LockResetIcon from '@mui/icons-material/LockReset';
 
 const SERVER_URL = "https://jar-coin-counter-backend.onrender.com";
 
@@ -12,6 +13,11 @@ function JarCoinCounter() {
     const handleIncrement = () => {
         axios.put(SERVER_URL + "/count");
         setCount(prev => prev + 1);
+    }
+
+    const handleReset = () => {
+        axios.put(SERVER_URL + "/reset");
+        setCount(0);
     }
 
     useEffect(() => {
@@ -24,7 +30,8 @@ function JarCoinCounter() {
             <div>
                 <Jar counter={count} />
             </div>
-            <div>
+            <div style={{ display: "flex", justifyContent: "center", alignContent: "center", gap: "20px" }}>
+                <div><Button color={"red"} name={"Reset"} icon={<LockResetIcon />} onCall={handleReset} /></div>
                 <div><Button color={"purple"} name={"Increment"} icon={<AddIcon />} onCall={handleIncrement} /></div>
             </div>
         </div>
