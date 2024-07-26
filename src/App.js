98b6e2e4b1f97a6f8e3e1f0f1f22a3729d23f5e1
Santhosh Kumar <santhoshkumar@Santhoshs-MacBook-Pro.local>
2024-07-26 17:52:45 +0530

    removed the unused imports

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import JarCoinCounter from './components/Jar/JarCoinCounter';
 
 function App() {
+  document.title = "Jar"
   return (
     <div className="App">
       <JarCoinCounter />
