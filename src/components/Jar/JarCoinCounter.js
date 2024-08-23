851b6649253627965b8895ffb21347cd84c07a00
gamingsanth <gamingsanth@gmail.com>
2024-08-23 16:46:55 +0530

    Update JarCoinCounter.js

--- src/components/Jar/JarCoinCounter.js
+++ src/components/Jar/JarCoinCounter.js
@@ -6,7 +6,7 @@ import axios from "axios";
 import LockResetIcon from '@mui/icons-material/LockReset';
 import { CircularProgress } from "@mui/material";
 
-const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+const SERVER_URL = "http://ec2-13-201-25-64.ap-south-1.compute.amazonaws.com";
 
 
 function JarCoinCounter() {
